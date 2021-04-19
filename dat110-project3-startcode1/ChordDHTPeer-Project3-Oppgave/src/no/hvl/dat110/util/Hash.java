e99c7036db3086b15d39847146fba0b37770d291
hav0r <halvor_98@hotmail.com>
2021-04-19 21:35:24 +0200

    Add files via upload

---
+++ dat110-project3-startcode1/ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
@@ -0,0 +1,88 @@
+package no.hvl.dat110.util;
+
+/**
+ * project 3
+ * @author tdoy
+ *
+ */
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Hash {
+
+	private static BigInteger hashint;
+
+	public static BigInteger hashOf(String entity) {
+
+		// Task: Hash a given string using MD5 and return the result as a BigInteger.
+
+		// we use MD5 with 128 bits digest
+
+		// compute the hash of the input 'entity'
+
+		// convert the hash into hex format
+
+		// convert the hex into BigInteger
+
+		// return the BigInteger
+
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("MD5");
+			byte[] mdbytes = md.digest(entity.getBytes("UTF-8"));
+			
+			String hex = toHex(mdbytes);
+			
+			hashint = new BigInteger(hex,16);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return hashint;
+	}
+
+	public static BigInteger addressSize() {
+
+		// Task: compute the address size of MD5
+
+		// get the digest length
+
+		// compute the number of bits = digest length * 8
+
+		// compute the address size = 2 ^ number of bits
+
+		// return the address size
+
+		BigInteger mod = new BigInteger("2");
+		mod = mod.pow(bitSize());
+
+		return mod;
+	}
+
+	public static int bitSize() {
+
+		int digestlen = 0;
+
+		// find the digest length
+		try {
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		digestlen = md.getDigestLength();
+		}catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return digestlen * 8;
+	}
+
+	public static String toHex(byte[] digest) {
+		StringBuilder strbuilder = new StringBuilder();
+		for (byte b : digest) {
+			strbuilder.append(String.format("%02x", b & 0xff));
+		}
+		return strbuilder.toString();
+	}
+
+}
